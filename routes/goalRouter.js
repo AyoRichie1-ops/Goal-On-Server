01d6a56a84e4902d774c3526e98f351a284aba10
AyoRichie1-ops <onanusiabdullateef@gmail.com>
2024-01-17 11:06:07 +0100

    first commit

---
+++ routes/goalRouter.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllGoals,
+  getAGoal,
+  createGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+// const { updateOne } = require("../models/goalsmodel");
+
+
+// router.get('/api/goals', getAllGoals);
+// router.post('/api/goals', createGoal);
+
+router.route('/').get(getAllGoals).post(createGoal)
+
+router.route('/:goalsId').get(getAGoal).patch(updateGoal).delete(deleteGoal)
+
+// router.get("/api/goals/:goalId", getAGoal)
+// router.patch("/api/goals/:goalId", updateGoal)
+// router.delete("/api/goals/:goalId", deleteGoal)
+
+module.exports = router;
